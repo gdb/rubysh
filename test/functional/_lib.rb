83f744b9078fc638e45a071249f6a90b8fb5408e
Greg Brockman <gdb@gregbrockman.com>
2013-11-28 16:39:20 -0800

    Get rid of unneeded MiniTest configuration

--- test/functional/_lib.rb
+++ test/functional/_lib.rb
@@ -3,5 +3,3 @@ require File.expand_path('../_lib', File.dirname(__FILE__))
 module RubyshTest::Functional
   class FunctionalTest < RubyshTest::Test; end
 end
-
-MiniTest::Unit.runner = MiniTest::Unit.new
--- test/unit/_lib.rb
+++ test/unit/_lib.rb
@@ -3,5 +3,3 @@ require File.expand_path('../_lib', File.dirname(__FILE__))
 module RubyshTest::Unit
   class UnitTest < RubyshTest::Test; end
 end
-
-MiniTest::Unit.runner = MiniTest::Unit.new
